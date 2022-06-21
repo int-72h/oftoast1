505903778ef75725d312913ca8d5eb9b6d3ef633
int-72h <int-10h@protonmail.com>
2022-06-21 22:30:50 +0100

    cli fix + partial update

--- ofjam/cli.py
+++ ofjam/cli.py
@@ -14,7 +14,7 @@ import httpx
 parser = argparse.ArgumentParser(description="Manage Open Fortress installation.")
 parser.add_argument("action", type=str, help='action to execute on a directory, currently only "upgrade"')
 parser.add_argument("directory", type=str, help='a directory to install it to.')
-parser.add_argument("-u", default="http://toast.openfortress.fun/toast/", help="url to fetch data from")
+parser.add_argument("-u", default="https://toast.openfortress.fun/toast/", help="url to fetch data from")
 parser.add_argument("-c", default="4", help="no. of threads")
 args = parser.parse_args()
 
@@ -24,24 +24,33 @@ if args.action != "upgrade":
 
 game_path = Path(args.directory)
 
+
+user_agent = 'murse/0.0.2'
+
+url = args.u
+response = httpx.get(url, headers={'user-agent': 'murse/0.0.2'}, follow_redirects=True)
+resUrl = response.url
+url = "https://" + resUrl.host + "/toast/"
+print("Server Selected: " + url)
+num_threads = httpx.get(url + "/reithreads", headers={'user-agent': user_agent}, follow_redirects=True).text
+latest_revision = fetch_latest_revision(url)
 installed_revision = get_installed_revision(game_path)
-latest_revision = fetch_latest_revision(args.u)
 
 print(installed_revision, "->", latest_revision, file=stderr)
 
-revisions = fetch_revisions(args.u, installed_revision, latest_revision)
+revisions = fetch_revisions(url, installed_revision, latest_revision)
 changes = replay_changes(revisions)
 
 
 
 writes = list(filter(lambda x: x["type"] == TYPE_WRITE, changes))
-executor = ThreadPoolExecutor(int(args.c))
+executor = ThreadPoolExecutor(int(num_threads))
 
 
 def work(x):
-    with httpx.Client(http2=True, headers={'user-agent': 'Mozilla/5.0', 'Connection': 'keep-alive', 'Cache-Control': 'max-age=0'}) as client:
+    with httpx.Client(http2=True, headers={'user-agent': 'murse/0.0.2', 'Connection': 'keep-alive', 'Cache-Control': 'max-age=0'}) as client:
         resp = client.get(args.u + "/objects/" + x["object"])
-        file = open(game_path / x["object"], "wb+")
+        file = open(game_path / x["path"], "wb+")
         file.write(resp.content)
         file.close()
 
