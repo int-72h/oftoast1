bdde0a845c98ef730491129eb6131159d0b49cc0
int-72h <int-10h@protonmail.com>
2022-06-23 14:54:21 +0100

    signing support added to cli

--- ofjam/cli.py
+++ ofjam/cli.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from tvn import *
+from pytoast import *
 import argparse
 import os
 from subprocess import Popen,PIPE
-
+from Crypto.PublicKey import ECC
 
 from pathlib import Path
 from sys import exit, stderr
@@ -15,6 +15,7 @@ parser.add_argument("action", type=str, help='action to execute on a directory, 
 parser.add_argument("directory", type=str, help='a directory to install it to.')
 parser.add_argument("-u", default="https://toast.openfortress.fun/toast/", help="url to fetch data from")
 parser.add_argument("-c", default="4", help="no. of threads")
+parser.add_argument("-k", default=None, help="keyfile")
 args = parser.parse_args()
 
 if args.action != "upgrade":
@@ -23,7 +24,11 @@ if args.action != "upgrade":
 
 game_path = Path(args.directory)
 
-
+if args.k:
+    f = open(args.k, 'r')
+    key = ECC.import_key(f.read())
+else:
+    key = None
 user_agent = 'murse/0.0.2'
 
 url = args.u
@@ -32,12 +37,12 @@ resUrl = response.url
 url = "https://" + resUrl.host + "/toast/"
 print("Server Selected: " + url)
 num_threads = httpx.get(url + "/reithreads", headers={'user-agent': user_agent}, follow_redirects=True).text
-latest_revision = fetch_latest_revision(url)
+latest_revision = fetch_latest_revision(url,key)
 installed_revision = get_installed_revision(game_path)
 
 print(installed_revision, "->", latest_revision, file=stderr)
 
-revisions = fetch_revisions(url, installed_revision, latest_revision)
+revisions = fetch_revisions(url, installed_revision, latest_revision,key)
 changes = replay_changes(revisions)
 
 
@@ -67,9 +72,9 @@ for x in list(filter(lambda x: x["type"] == TYPE_MKDIR, changes)):
 todl = open('todl.txt','w')
 for x in writes:
     if x["type"] == TYPE_WRITE:
-        todl.write('{}\n out={}\n checksum=md5={}\n'.format(url+"objects/"+x["object"], game_path / str(x["path"]), x["hash"]))
+        todl.write('{}\n out={}\n checksum=sha-256={}\n'.format(url+"objects/"+x["object"], game_path / str(x["path"]), x["hash"]))
 todl.close()
-fp = Popen('aria2c -i todl.txt -d . -x 12 -j 100 -m 10 -V -U murse/0.0.2', shell=True,stdin=PIPE, stdout=PIPE, universal_newlines=True)
+fp = Popen('aria2c -i todl.txt -d / -x 100 -j 100 -m 10 -V -U murse/0.0.2', shell=True,stdin=PIPE, stdout=PIPE, universal_newlines=True)
 for line in fp.stdout:
     print(line)
 
